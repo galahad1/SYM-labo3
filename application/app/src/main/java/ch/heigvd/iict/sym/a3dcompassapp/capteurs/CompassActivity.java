6c37a3d2d00544c2ab8830124bed87748c7ff07d
Tano Iannetta <tano.iannetta@heig-vd.ch>
2017-12-01 22:17:57 +0100

    premier essaye, rien se passe =(

--- application/app/src/main/java/ch/heigvd/iict/sym/a3dcompassapp/capteurs/CompassActivity.java
+++ application/app/src/main/java/ch/heigvd/iict/sym/a3dcompassapp/capteurs/CompassActivity.java
@@ -1,5 +1,9 @@
 package ch.heigvd.iict.sym.a3dcompassapp.capteurs;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.opengl.GLSurfaceView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,11 +12,34 @@ import android.view.WindowManager;
 
 import ch.heigvd.iict.sym.a3dcompassapp.R;
 
-public class CompassActivity extends AppCompatActivity {
+public class CompassActivity extends AppCompatActivity implements SensorEventListener {
 
     //opengl
     private OpenGLRenderer opglr           = null;
     private GLSurfaceView   m3DView         = null;
+
+
+    private int mAzimuth = 0; // degree
+
+    //sensors
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+    private Sensor magnometer;
+
+
+    //tab
+    private float[] accelerometerReading = new float[3];
+    private float[] magnetometerReading = new float[3];
+    private float[] rotationMatrix = new float[9];
+    private float[] rMat = new float[9];
+    private float[] iMat = new float[9];
+    private float[] orientationAngles = new float[3];
+
+
+    public CompassActivity() {
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +62,56 @@ public class CompassActivity extends AppCompatActivity {
         this.m3DView.setRenderer(this.opglr);
 
     }
+
+    protected void onResume() {
+        super.onResume();
+
+        this.sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        this.accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        this.magnometer = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, magnometer, SensorManager.SENSOR_DELAY_NORMAL);
+
+        //sensorManager.registerListener(this, Sensor.TYPE_ACCELEROMETER, SensorManager.SENSOR_DELAY_NORMAL, SensorManager.SENSOR_DELAY_UI);
+        //sensorManager.registerListener(this, Sensor.TYPE_MAGNETIC_FIELD, SensorManager.SENSOR_DELAY_NORMAL, SensorManager.SENSOR_DELAY_UI);
+
+
+
+
+    }
+
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+
+        float[] data;
+        switch ( sensorEvent.sensor.getType() ) {
+            case Sensor.TYPE_ACCELEROMETER:
+                accelerometerReading = sensorEvent.values.clone();
+                break;
+            case Sensor.TYPE_MAGNETIC_FIELD:
+                magnetometerReading = sensorEvent.values.clone();
+                break;
+            default: return;
+        }
+
+        if ( SensorManager.getRotationMatrix( rMat, iMat, accelerometerReading, magnetometerReading ) ) {
+            mAzimuth= (int) ( Math.toDegrees( SensorManager.getOrientation( rMat, orientationAngles )[0] ) + 360 ) % 360;
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
 
     /* TODO */
     // your activity need to register accelerometer and magnetometer sensors' updates
