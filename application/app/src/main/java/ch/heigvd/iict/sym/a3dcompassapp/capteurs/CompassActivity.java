4e909160810db2839898668a04faf8c757671ed0
Tano Iannetta <tano.iannetta@heig-vd.ch>
2017-12-08 15:05:55 +0100

    compass activity finished

--- application/app/src/main/java/ch/heigvd/iict/sym/a3dcompassapp/capteurs/CompassActivity.java
+++ application/app/src/main/java/ch/heigvd/iict/sym/a3dcompassapp/capteurs/CompassActivity.java
@@ -1,5 +1,6 @@
 package ch.heigvd.iict.sym.a3dcompassapp.capteurs;
 
+import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -12,14 +13,15 @@ import android.view.WindowManager;
 
 import ch.heigvd.iict.sym.a3dcompassapp.R;
 
+/**
+ * This class implements a 3D compass that display magnetic north direction
+ * Modified by Tano Iannetta, Lara Chauffoureaux, Wojciech Myszkorowski
+ */
 public class CompassActivity extends AppCompatActivity implements SensorEventListener {
 
     //opengl
     private OpenGLRenderer opglr           = null;
-    private GLSurfaceView   m3DView         = null;
-
-
-    private int mAzimuth = 0; // degree
+    private GLSurfaceView  m3DView         = null;
 
     //sensors
     private SensorManager sensorManager;
@@ -27,19 +29,15 @@ public class CompassActivity extends AppCompatActivity implements SensorEventLis
     private Sensor magnometer;
 
 
-    //tab
-    private float[] accelerometerReading = new float[3];
-    private float[] magnetometerReading = new float[3];
-    private float[] rotationMatrix = new float[9];
-    private float[] rMat = new float[9];
-    private float[] iMat = new float[9];
-    private float[] orientationAngles = new float[3];
+    // sensors values
+    private float[] accelerometerReading = null;
+    private float[] magnetometerReading = null;
+
+    // matrix
+    private float[] rotationMatrix = new float[16];
 
 
-    public CompassActivity() {
-
-    }
-
+    public CompassActivity() {}
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,23 +59,17 @@ public class CompassActivity extends AppCompatActivity implements SensorEventLis
         //init opengl surface view
         this.m3DView.setRenderer(this.opglr);
 
+        // sensors
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        this.accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        this.magnometer = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+
     }
 
     protected void onResume() {
         super.onResume();
-
-        this.sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
-        this.accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        this.magnometer = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
-
-        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-        sensorManager.registerListener(this, magnometer, SensorManager.SENSOR_DELAY_NORMAL);
-
-        //sensorManager.registerListener(this, Sensor.TYPE_ACCELEROMETER, SensorManager.SENSOR_DELAY_NORMAL, SensorManager.SENSOR_DELAY_UI);
-        //sensorManager.registerListener(this, Sensor.TYPE_MAGNETIC_FIELD, SensorManager.SENSOR_DELAY_NORMAL, SensorManager.SENSOR_DELAY_UI);
-
-
-
+        sensorManager.registerListener(this, magnometer, SensorManager.SENSOR_DELAY_GAME );
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_GAME);
 
     }
 
@@ -91,19 +83,19 @@ public class CompassActivity extends AppCompatActivity implements SensorEventLis
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
 
-        float[] data;
-        switch ( sensorEvent.sensor.getType() ) {
-            case Sensor.TYPE_ACCELEROMETER:
-                accelerometerReading = sensorEvent.values.clone();
-                break;
-            case Sensor.TYPE_MAGNETIC_FIELD:
-                magnetometerReading = sensorEvent.values.clone();
-                break;
-            default: return;
+        // new values
+       if (sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            accelerometerReading = sensorEvent.values;
+        }
+        else if (sensorEvent.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+           magnetometerReading = sensorEvent.values;
         }
 
-        if ( SensorManager.getRotationMatrix( rMat, iMat, accelerometerReading, magnetometerReading ) ) {
-            mAzimuth= (int) ( Math.toDegrees( SensorManager.getOrientation( rMat, orientationAngles )[0] ) + 360 ) % 360;
+        if (accelerometerReading != null && magnetometerReading != null)
+        {
+            // calculus and display
+            SensorManager.getRotationMatrix(rotationMatrix, null,accelerometerReading,magnetometerReading);
+            this.opglr.swapRotMatrix(rotationMatrix);
         }
     }
 
@@ -111,14 +103,4 @@ public class CompassActivity extends AppCompatActivity implements SensorEventLis
     public void onAccuracyChanged(Sensor sensor, int i) {
 
     }
-
-
-    /* TODO */
-    // your activity need to register accelerometer and magnetometer sensors' updates
-    // then you may want to call
-    //  this.opglr.swapRotMatrix()
-    // with the 4x4 rotation matrix, everytime a new matrix is computed
-    // more information on rotation matrix can be found on-line:
-    // https://developer.android.com/reference/android/hardware/SensorManager.html#getRotationMatrix(float[],%20float[],%20float[],%20float[])
-
 }
